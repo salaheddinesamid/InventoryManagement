e2bf93f994abb1f8d91218a3b128dfb9b5da504e
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-03-18 22:12:10 +0000

    commenting code

--- Backend/inventorymanagement/src/main/java/com/inventory/application/controllers/ProductsController.java
+++ Backend/inventorymanagement/src/main/java/com/inventory/application/controllers/ProductsController.java
@@ -20,23 +20,28 @@ public class ProductsController {
     ProductsController(ProductService productService){
         this.productService = productService;
     }
-
+    // Handling GET requests and providing data about all products:
     @GetMapping("/")
     List<Product> getAllProducts(){
         return productService.filterProducts();
     }
+    // Handling GET request for getting the total number of products
     @GetMapping("/total")
     int getTotal(){
+        // Uses a service from Product Service
         return productService.totalProducts();
     }
+    // Handling POST requests to add new products into the system
     @PostMapping("/newproduct")
     ResponseEntity<Object> newProduct(@RequestBody Product product){
         return productService.addNewProduct(product);
     }
+    // Handling PUT requests if the manager wants to update information about any product
     @RequestMapping(value = "/update",
         method = RequestMethod.PUT,
         produces = MediaType.APPLICATION_JSON_VALUE)
     Product updateProduct(@RequestBody PutRequest putRequest){
+
         return productService.update(putRequest.getId(),putRequest.getNewPrice(),putRequest.getNewQuantity(),putRequest.getNewStatus());
     }
 
