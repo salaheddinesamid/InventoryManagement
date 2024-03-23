95142be4b5a0c752fb2ee534dff7d8bd2704e620
salaheddinesamid <salaheddine.kobra@iu-study.org>
2024-03-23 17:23:25 +0000

    fixing bugs

--- Backend/inventorymanagement/src/main/java/com/inventory/application/model/Customer.java
+++ Backend/inventorymanagement/src/main/java/com/inventory/application/model/Customer.java
@@ -1,2 +1,65 @@
-package com.inventory.application.model;public class Customer {
+package com.inventory.application.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    Long id;
+    private String fullName;
+    private String email;
+    private int age;
+    private String address;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
